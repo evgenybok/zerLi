4d1a0a3509a0ed7c04b81b4818ed4af679a9509b
evbo <Evgeny.Boka@e.braude.ac.il>
2022-05-03 11:27:22 +0300

    ZerliShared update

--- ZerliClient/Zerli-Client/communication/MessageAnswer.java
+++
@@ -1,8 +0,0 @@
-package communication;
-
-public enum MessageAnswer {
-
-    WAIT_RESPONSE, SUCCEED, NOT_SUCCEED, APPROVED
-
-}
-
--- ZerliClient/Zerli-Client/communication/MessageType.java
+++
@@ -1,8 +0,0 @@
-package communication;
-
-public enum MessageType {
-
-    /*Message from Server*/
-    CONFIRM_IP, WAIT_RESPONSE, SUCCEED, NOT_SUCCEED, ERROR, EXIT, LOGIN,LOGOUT, UPDATE, GET_ORDERS;
-
-}
